1622fbb2bd483a21c243229aaa9a7570a8b78831
khaoulachaieb <khaoula.chaieb@esprit.tn>
2024-03-02 13:18:52 +0100

    Khaoula First commit

---
+++ demo/src/main/java/com/group6/nombrepremier.java
@@ -0,0 +1,30 @@
+public class NombrePremier {
+    
+    // Méthode pour vérifier si un nombre est premier
+    static boolean estPremier(int nombre) {
+        // Si le nombre est inférieur ou égal à 1, il n'est pas premier
+        if (nombre <= 1) {
+            return false;
+        }
+        
+        // Vérification si le nombre est divisible par un nombre autre que 1 et lui-même
+        for (int i = 2; i < nombre; i++) {
+            if (nombre % i == 0) {
+                return false;
+            }
+        }
+        
+        // Si aucune condition précédente n'est satisfaite, le nombre est premier
+        return true;
+    }
+    
+    public static void main(String[] args) {
+        int nombre = 17; // Mettez ici le nombre que vous voulez vérifier
+        
+        if (estPremier(nombre)) {
+            System.out.println(nombre + " est un nombre premier.");
+        } else {
+            System.out.println(nombre + " n'est pas un nombre premier.");
+        }
+    }
+}
---
+++ demo/src/main/java/com/group6/puissance.java
@@ -0,0 +1,15 @@
+public class CalculPuissance {
+    
+    // Méthode pour calculer la puissance d'un nombre
+    static double puissance(double nombre, int exposant) {
+        return Math.pow(nombre, exposant);
+    }
+    
+    public static void main(String[] args) {
+        double nombre = 2; // Mettez ici le nombre
+        int exposant = 3; // Mettez ici l'exposant
+        
+        double resultat = puissance(nombre, exposant);
+        System.out.println(nombre + " à la puissance " + exposant + " = " + resultat);
+    }
+}
