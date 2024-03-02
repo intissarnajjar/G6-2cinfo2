c3b61957873f0c1eeba38fa51ebb18b619df5d93
khaoulachaieb <khaoula.chaieb@esprit.tn>
2024-03-02 14:09:57 +0100

    modifier modifier

--- demo/src/main/Pair.java
+++ demo/src/main/Pair.java
@@ -4,7 +4,7 @@ public class VerifierNombrePair {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Entrez un nombre : ");
+        System.out.print("Entrez un numéro : ");
         int nombre = scanner.nextInt();
 
         System.out.println(nombre % 2 == 0 ? "Nombre pair." : "Nombre impair.");
